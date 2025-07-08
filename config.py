8d7c51666e2f2435e652c03e262fff0a049df4b7
marko11-w <69992888+marko11-w@users.noreply.github.com>
2025-07-08 16:25:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 TOKEN = "7863510154:AAHYUQn1pWaNfyA9zrdPIHIjcCb8nB2pz58"
 WEBHOOK_URL = "https://pubg-production.up.railway.app/"
-CHANNEL_USERNAME = "@MARK01i"  # غيّره إذا كان مختلف
+CHANNEL_USERNAME = "@MARK01i"
 ADMIN_IDS = [7758666677]
 DEFAULT_TEXT = "📌 النسخة الخاصة بنا"
 DATA_FOLDER = "data"
